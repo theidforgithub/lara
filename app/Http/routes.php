6f05d9ca83cf7fb92ce47da85a13c1daf32073f3
theidforgithub <alwaysreceive@gmail.com>
2016-01-29 14:54:24 +0800

    新增auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,8 @@ Route::group(['middleware' => 'web'], function () {
 	});
 
 });
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
